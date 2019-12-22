afa09cf040850288b7e025838853fc73983eada3
curgery <androidgeek54@gmail.com>
2019-12-22 15:09:19 -0500

    updated the game logic to completion

--- App.js
+++ App.js
@@ -1,41 +1,98 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Button, FlatList } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
+// import GoalItem from './components/GoalItem';
+// import GoalInput from './components/GoalInput';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false);
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
 
-  const addGoalHandler = goalTitle => {
-    setCourseGoals(currentGoals => [
-      ...currentGoals,
-      { id: Math.random().toString(), value: goalTitle }
-    ]);
-    setIsAddMode(false);
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={err => console.log(err)}
+      />
+    );
+  }
+
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  };
+  // const [courseGoals, setCourseGoals] = useState([]);
+  // const [isAddMode, setIsAddMode] = useState(false);
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0);
   };
 
-  const removeGoalHandler = goalId => {
-    setCourseGoals(currentGoals => {
-      return currentGoals.filter(goal => goal.id !== goalId);
-    });
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
   };
 
-  const cancelGoalAdditionHandler = () => {
-    setIsAddMode(false);
-  };
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
+  }
+
+  // const addGoalHandler = goalTitle => {
+  //   setCourseGoals(currentGoals => [
+  //     ...currentGoals,
+  //     { id: Math.random().toString(), value: goalTitle }
+  //   ]);
+  //   setIsAddMode(false);
+  // };
+
+  // const removeGoalHandler = goalId => {
+  //   setCourseGoals(currentGoals => {
+  //     return currentGoals.filter(goal => goal.id !== goalId);
+  //   });
+  // };
+
+  // const cancelGoalAdditionHandler = () => {
+  //   setIsAddMode(false);
+  // };
 
   return (
     <View style={styles.screen}>
-      <Header title='Oh Happy Day Warehouse' />
-      <StartGameScreen />
-      <Button
+      <Header title='Guess a Number' />
+      {content}
+      {/* <StartGameScreen />
+      <GameScreen /> */}
+      {/* <Button
         title='Add New Inventory Item'
         onPress={() => setIsAddMode(true)}
-      />
-      <GoalInput
+      /> */}
+      {/* <GoalInput
         visible={isAddMode}
         onAddGoal={addGoalHandler}
         onCancel={cancelGoalAdditionHandler}
@@ -50,7 +107,7 @@ export default function App() {
             title={itemData.item.value}
           />
         )}
-      />
+      /> */}
     </View>
   );
 }
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,19 +5,60 @@ import {
   StyleSheet,
   Button,
   TouchableWithoutFeedback,
-  Keyboard
+  Keyboard,
+  Alert
 } from 'react-native';
 
 import Card from '../components/Card';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
+import BodyText from '../components/BodyText';
 import Colors from '../constants/colors';
 
 const StartGameScreen = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
 
   const numberInputHandler = inputText => {
     setEnteredValue(inputText.replace(/[^0-9]/g, ''));
   };
+
+  const resetInputHandler = () => {
+    setEnteredValue('');
+    setConfirmed(false);
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid number!',
+        'Number has to be a number between 1 and 99.',
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+      );
+      return;
+    }
+    setConfirmed(true);
+    setSelectedNumber(chosenNumber);
+    setEnteredValue('');
+    Keyboard.dismiss();
+  };
+
+  let confirmedOutput;
+
+  if (confirmed) {
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <BodyText>You selected</BodyText>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button
+          title='START GAME'
+          onPress={() => props.onStartGame(selectedNumber)}
+        />
+      </Card>
+    );
+  }
 
   return (
     <TouchableWithoutFeedback
@@ -26,9 +67,9 @@ const StartGameScreen = props => {
       }}
     >
       <View style={styles.screen}>
-        <Text style={styles.title}>Add Inventory Item</Text>
+        <Text style={styles.title}>Start a New Game!</Text>
         <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
+          <BodyText>Select a Number</BodyText>
           <Input
             style={styles.input}
             blurOnSubmit
@@ -41,13 +82,22 @@ const StartGameScreen = props => {
           />
           <View style={styles.buttonContainer}>
             <View style={styles.button}>
-              <Button title='Reset' onPress={() => {}} color={Colors.accent} />
+              <Button
+                title='Reset'
+                onPress={resetInputHandler}
+                color={Colors.accent}
+              />
             </View>
             <View style={styles.button}>
-              <Button title='Confirm' color={Colors.primary} />
+              <Button
+                title='Confirm'
+                onPress={confirmInputHandler}
+                color={Colors.primary}
+              />
             </View>
           </View>
         </Card>
+        {confirmedOutput}
       </View>
     </TouchableWithoutFeedback>
   );
@@ -61,7 +111,8 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    marginVertical: 10
+    marginVertical: 10,
+    fontFamily: 'open-sans-bold'
   },
   inputContainer: {
     width: 300,
@@ -80,6 +131,10 @@ const styles = StyleSheet.create({
   input: {
     width: 50,
     textAlign: 'center'
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: 'center'
   }
 });
 
